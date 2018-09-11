7b58a2c20fce99a65958d6319bf92df5c24bdcf1
Jason Cunning <jcunning@ucar.edu>
2018-09-10 21:41:13 -0600

    Add Creative Bass

--- yellowstone_backup.sh
+++ yellowstone_backup.sh
@@ -2,6 +2,7 @@
 
 rsync -av /Users/jcunning/Pictures/JIM/SicPics "/Volumes/Yellowstone HD/Backup/Pictures/JIM"
 rsync -av /Users/jcunning/Pictures/Photos\ Library.photoslibrary "/Volumes/Yellowstone HD/Backup/Pictures"
+rsync -av /Users/jcunning/Guitar/CreativeBass/*.* "/Volumes/Yellowstone HD/Backup/Guitar/CreativeBass"
 rsync -av /Users/jcunning/Documents/PersonalSrc "/Volumes/Yellowstone HD/Backup/Documents"
 rsync -av /Users/jcunning/Documents/Misc/*.* "/Volumes/Yellowstone HD/Backup/Documents/Misc"
 rsync -av /Users/jcunning/Documents/Budget/Budget\ 20??.ods "/Volumes/Yellowstone HD/Backup/Documents/Budget"
